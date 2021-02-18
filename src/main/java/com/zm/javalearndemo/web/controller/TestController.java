efc6238ae1365321dfc66a5b473211746b7ed299
dxiaoping <1693547683@qq.com>
2021-02-18 14:30:05 +0800

    案例初始化

---
+++ src/main/java/com/zm/javalearndemo/web/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.zm.javalearndemo.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Tile: NTS20110100148  广州一体化项目Oracle环境测试、问题修复
+ * @Author: xiaoping.duan 医保研发5部 CN32219
+ * @Date 2021/2/18
+ */
+@RestController
+@RequestMapping("/java-learn/test")
+
+public class TestController {
+    @GetMapping("sayHello")
+    public String sayHello(){
+        return "Hello";
+    }
+}
---
+++ src/test/java/com/zm/javalearndemo/JavaLearnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zm.javalearndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearnDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
